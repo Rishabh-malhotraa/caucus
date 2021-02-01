20d7e423d8033967ede1ef356fe964f71cff30d4
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2021-02-01 23:21:51 +0530

    styling 💅🏻

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -143,7 +143,10 @@ const LoginII = () => {
                   <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
                 </svg>
               </Button>
-              <div className={classes['alternate-box']}>
+              <button
+                className={classes['alternate-box']}
+                style={{ backgroundColor: 'white' }}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className={[
@@ -164,7 +167,7 @@ const LoginII = () => {
                   <path d="M9 7c-3 0 -4 3 -4 5.5c0 3 2 7.5 4 7.5c1.088 -.046 1.679 -.5 3 -.5c1.312 0 1.5 .5 3 .5s4 -3 4 -5c-.028 -.01 -2.472 -.403 -2.5 -3c-.019 -2.17 2.416 -2.954 2.5 -3c-1.023 -1.492 -2.951 -1.963 -3.5 -2c-1.433 -.111 -2.83 1 -3.5 1c-.68 0 -1.9 -1 -3 -1z" />
                   <path d="M12 4a2 2 0 0 0 2 -2a2 2 0 0 0 -2 2" />
                 </svg>
-              </div>
+              </button>
             </div>
           </div>
         </div>
