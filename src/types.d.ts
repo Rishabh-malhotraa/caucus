e155ad836356a3f92dfc96b2ed5ca410532764a7
Kundan <starkkumarkk1611@gmail.com>
2021-10-06 13:56:26 +0530

    ScrappedDataType inteface updated in types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -83,7 +83,7 @@ export interface QuestionDataSS {
 }
 export interface ScrappedDataType {
   htmlString: string;
-  hostname: "codeforces.com" | "atcoder.jp";
+  hostname: "codeforces.com" | "atcoder.jp" | "cses.fi";
 }
 
 export interface TabsContextTypes {
